2e48026512be21e2b6ab7846ef33b321a122fdc3
dharshinipriya-k <priyadk1809@gmail.com>
2023-11-03 15:43:08 +0530

    Question 4,5 updated

--- script.js
+++ script.js
@@ -6,14 +6,34 @@ country_api.onload = function(){
     var data = country_api.response;
     var result = JSON.parse(data);
 // TASK 1 ------------------
-    console.log("--------------Countries in Asia-------------");
+    console.log("--------------COUNTRIES IN ASIA CONTINENT-------------");
 
     var res1 = result.filter((x)=>x.region == "Asia")
-    res1.map((ele)=>console.log(ele.name.common))
+    res1.forEach((ele)=>console.log(ele.name.common))
 //TASK 2 ---------------------
-    console.log("--------------Countries having population < 2 lakhs---------");
+    console.log("--------------COUNTRIES HAVING POPULATION < 2 LAKHS---------");
 
     var res2 = result.filter((y)=>y.population<200000);
-    res2.map((ele)=>console.log(ele.name.common));
+    res2.forEach((ele)=>console.log(ele.name.common));
+
+    //TASK 3-----------
+    console.log("---------------------PRINTING USING FOR EACH-------------------------------");
+    var res3 = result.forEach(element => {
+        console.log("Country_name:",element.name.common,"  Capital:",element.capital,"Flag:",element.flags.png);});
+
+     //TASK 4-----------
+    console.log("---------------------TOTAL POPULATION USING REDUCE---------------");
+    var pop = result.reduce((total,i)=>{
+       return total+i.population;
+    },0)
+    console.log("Total Population:",pop);
+    
+    //TASK 5------------------
+    console.log("-------------------COUNTRIES THAT USE US DOLLARS-----------");
+
+    var usd = result.filter(ele=>ele.currencies && ele.currencies.USD);
+    usd.forEach(ele=>console.log(ele.name.common));
+
+
 }
 
